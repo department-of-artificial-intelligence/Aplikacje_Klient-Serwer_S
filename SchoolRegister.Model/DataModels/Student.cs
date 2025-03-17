247fa929e820774dee869452cd729af70023e3c2
smth <smth@smth.smth>
2025-03-17 22:06:37 +0100

    Poprawka

--- SchoolRegister.Model/DataModels/Student.cs
+++ SchoolRegister.Model/DataModels/Student.cs
@@ -17,8 +17,15 @@ public class Student : User
     public Parent Parent {get; set;}= null!;
     
     public int ParentId {get; set;}
-    public double AverageGrade {get;}
-    public IDictionary<string, double> AverageGradePerSubject {get;}= null!;
-    public IDictionary<string, List<GradeScale>> GradesPerSubject {get;} = null!;
+    public double AverageGrade => Grades?.Any() == true ? Grades.Average(g => (int)g.GradeValue) : 0;
+    public IDictionary<string, double> AverageGradePerSubject => 
+    Grades?
+        .GroupBy(g => g.Subject.Name)
+        .ToDictionary(g => g.Key, g => g.Average(grade => (int)grade.GradeValue)) ?? new Dictionary<string, double>();
+    public IDictionary<string, List<GradeScale>> GradesPerSubject => 
+    Grades?
+        .GroupBy(g => g.Subject.Name)
+        .ToDictionary(g => g.Key, g => g.Select(grade => grade.GradeValue).ToList()) ?? new Dictionary<string, List<GradeScale>>();
+
 
 }
