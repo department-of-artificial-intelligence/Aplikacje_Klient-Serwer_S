521dff0a41cb8bf3c5cf5840b6795bb4629fdfb1
Antoni_Purgal <smth@smth.smth>
2025-04-07 21:08:44 +0200

    Dodanie AppDbContext

--- SchoolRegister.Model/DataModels/SubjectGroup.cs
+++ SchoolRegister.Model/DataModels/SubjectGroup.cs
@@ -2,7 +2,7 @@ namespace SchoolRegister.Model.DataModels{
     public class SubjectGroup{
         public Subject Subject {get; set;} = null!;
         public int SubjectId {get; set;}
-        public Group Group {get; set;} = null!;
+        public virtual Group Group {get; set;} = null!;
         public int GroupId {get; set;}
 
         public SubjectGroup() {}
---
+++ SchoolRegister.ViewModels/VM/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace SchoolRegister.ViewModel.VM{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+    
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
+
