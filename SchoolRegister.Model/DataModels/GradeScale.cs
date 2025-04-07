92e5b9dedd9ec4507b517e56590b5b422ef66f59
Marcin_Rybica <mr@gmail.com>
2025-04-08 00:26:42 +0200

    lab1 skonczony

---
+++ SchoolRegister.Model/DataModels/GradeScale.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace SchoolRegister.Model.DataModels;
+
+public enum GradeScale : int
+{
+    NDST =2,
+    DST=3,
+    DB = 4,
+    BDB =5
+}
---
+++ SchoolRegister.Model/DataModels/Teacher.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace SchoolRegister.Model.DataModels;
+
+public class Teacher : User
+{
+public string Title { get; set; } =null!;
+  
+public IList<Subject> Subjects {get;set;} = new List<Subject>();
+}
