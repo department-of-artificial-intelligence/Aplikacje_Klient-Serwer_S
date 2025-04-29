784a29930f6a8787ff07fb0f8d09ac69c7a20732
Emil Klimek <smth@smth.com>
2025-04-29 11:36:25 +0200

    Lab 3 -SOA, Building Services, AutoMapper, Software testing, Unit Tests

--- SchoolRegister.Model/DataModels/Subject.cs
+++ SchoolRegister.Model/DataModels/Subject.cs
@@ -4,21 +4,20 @@ using System.ComponentModel.DataAnnotations.Schema;
 public class Subject
 {
     [Key]
-    public int Id {get; set; }
+    public int Id { get; set; }
 
     [Required]
-    public string Name {get; set;} = null!;
+    public string Name { get; set; } = null!;
 
-    public string Description {get;set;} = null!;
+    public string Description { get; set; } = null!;
 
-    
-    public virtual IList<SubjectGroup> SubjectGroups {get; set;} = null!;
+    public virtual IList<SubjectGroup> SubjectGroups { get; set; } = null!;
 
-    public virtual Teacher Teacher {get;set;} = null!;
+    public virtual Teacher Teacher { get; set; } = null!;
 
-    [ForeignKey ("Teacher")]
-    public int? TeacherId {get;set;}
+    [ForeignKey("Teacher")]
+    public int? TeacherId { get; set; }
 
     [NotMapped]
-    public virtual IList<Grade> Grades{get;set;} = null!;
+    public virtual IList<Grade> Grades { get; set; } = null!;
 }
