d090f7939010c3d18f9e264482d816d2939e6154
Marcin_Rybica <mr@gmail.com>
2025-05-06 11:13:19 +0200

    lab3_przedTestami

---
+++ SchoolRegister.Services/ConcreteServices/BaseService.cs
@@ -0,0 +1,15 @@
+namespace SchoolRegister.Services.ConcreteServices
+{
+    public abstract class BaseService
+    {
+        protected readonly ApplicationDbContext DbContext = null!;
+        protected readonly ILogger Logger = null!;
+        protected readonly IMapper = null!;
+        public BaseService(ApplicationDbContext dbContext, IMapper mapper, ILogger logger)
+        {
+            DbContext = dbContext;
+            Logger = logger;
+            Mapper = mapper;
+        }
+    }
+}
