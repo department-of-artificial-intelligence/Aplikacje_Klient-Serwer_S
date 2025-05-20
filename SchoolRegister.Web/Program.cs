72e3be603b8ce3845a5f7d4585e734cd579dbf93
Antoni Purgal <smth@smth.smth>
2025-05-20 10:53:29 +0200

    Testnalabach

--- SchoolRegister.Web/Program.cs
+++ SchoolRegister.Web/Program.cs
@@ -3,6 +3,10 @@ using Microsoft.EntityFrameworkCore;
 using SchoolRegister.DAL.EF;
 using SchoolRegister.Model.DataModels;
 using SchoolRegister.Services.Configuration.AutoMapperProfiles;
+using Microsoft.Extensions.Localization;
+using SchoolRegister.Services.Interfaces;
+using SchoolRegister.Services.ConcreteServices;
+using SchoolRegister.Web.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +24,14 @@ builder.Services.AddDefaultIdentity<User>(options =>
     .AddUserManager<UserManager<User>>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
+builder.Services.AddTransient (typeof (ILogger), typeof (Logger<Program>));
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IStringLocalizer, StringLocalizer<BaseController>> ();
+builder.Services.AddScoped<ISubjectService, SubjectService> ();
+builder.Services.AddScoped<IGradeService, GradeService> ();
+builder.Services.AddScoped<IGroupService, GroupService> ();
+builder.Services.AddScoped<IStudentService, StudentService> ();
+builder.Services.AddScoped<ITeacherService, TeacherService> ();
 
 var app = builder.Build();
 
