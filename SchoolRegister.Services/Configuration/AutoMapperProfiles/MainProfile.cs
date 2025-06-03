1e696bcbf8f0788d22ef49aabe95ffe64a4f18af
Emil Klimek <mail@smth.com>
2025-06-03 11:14:14 +0200

    lab 04

--- SchoolRegister.Services/Configuration/AutoMapperProfiles/MainProfile.cs
+++ SchoolRegister.Services/Configuration/AutoMapperProfiles/MainProfile.cs
@@ -55,6 +55,19 @@ public class MainProfile : Profile
         CreateMap<Teacher, TeacherVm>();
         CreateMap<SubjectGroup, GroupVm>();
         
-        
+        //... previous maps in MainProfile constructor
+        CreateMap<RegisterNewUserVm, User>()
+            .ForMember(dest => dest.UserName, y => y.MapFrom(src => src.Email))
+            .ForMember(dest => dest.RegistrationDate, y => y.MapFrom(src => DateTime.Now));
+        CreateMap<RegisterNewUserVm, Parent>()
+            .ForMember(dest => dest.UserName, y => y.MapFrom(src => src.Email))
+            .ForMember(dest => dest.RegistrationDate, y => y.MapFrom(src => DateTime.Now));
+        CreateMap<RegisterNewUserVm, Student>()
+            .ForMember(dest => dest.UserName, y => y.MapFrom(src => src.Email))
+            .ForMember(dest => dest.RegistrationDate, y => y.MapFrom(src => DateTime.Now));
+        CreateMap<RegisterNewUserVm, Teacher>()
+            .ForMember(dest => dest.UserName, y => y.MapFrom(src => src.Email))
+            .ForMember(dest => dest.RegistrationDate, y => y.MapFrom(src => DateTime.Now))
+            .ForMember(dest => dest.Title, y => y.MapFrom(src => src.TeacherTitles));
     }
 }
