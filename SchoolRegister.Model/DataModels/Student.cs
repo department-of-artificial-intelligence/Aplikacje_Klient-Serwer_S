78a919e8792e5bf76cb4a8c80a5b495940dd12f7
Mateusz Praski <prask1.m3@gmail.com>
2026-03-09 08:18:03 +0100

    Logika do AverageGrade

--- SchoolRegister.Model/DataModels/Student.cs
+++ SchoolRegister.Model/DataModels/Student.cs
@@ -6,10 +6,16 @@ public class Student : User
 {
     public Group Group { get; set; } = null!;
     public int? GroupId { get; set; } = null!;
-    public IList<Grade> Grades { get; set; } = null!;
+    public IList<Grade> Grades { get; set; } = new List<Grade>();
     public Parent Parent { get; set; } = null!;
     public int? ParentId { get; set; } = null!;
-    public double AverageGrade { get; }
+    public double AverageGrade
+    {
+        get
+        {
+            Grades.Average(g => (double)g.GradeValue, 2);
+        }
+    }
     public IDictionary<string, double> AverageGradePerSubject { get; } = null!;
     public IDictionary<string, List<GradeScale>> GradePerSubject { get; } = null!;
 
