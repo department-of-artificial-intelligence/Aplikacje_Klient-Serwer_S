6fb305b1889fb51255ecfe8c7a6e4ba3d4908e81
Patryk_Magdziarz <smth@smth.smth>
2026-03-10 13:27:24 +0100

    add student logic

--- SchoolRegister.Model/DataModels/Student.cs
+++ SchoolRegister.Model/DataModels/Student.cs
@@ -10,26 +10,38 @@ public class Student : User
 
     public IList<Grade> Grades { get; set; }
 
-    public double AverageGrade { 
-        get{
-            //zrobić zwracanie 
+    public double AverageGrade
+    {
+        get
+        {
+            if (Grades == null)
+            {
+                return 0;
+            }
+
+            double avg = Grades.Average(x => (int)x.GradeValue);
+            return avg;
         }
     }
-    public IDictionary<string, double> AverageGradePerSubject { 
-        get{
-            //zrobić zwracanie
+    public IDictionary<string, double> AverageGradePerSubject
+    {
+        get
+        {
+            IDictionary<string, double> avgps = Grades.GroupBy(x => x.Subject.Name).ToDictionary(x => x.Key, x => x.Average(x => (int)x.GradeValue));
+            return avgps;
         }
     }
-    public IDictionary<string, List<GradeScale>> GradesPerSubject { 
-        get{
-            //zrobić zwracanie 
+    public IDictionary<string, List<GradeScale>> GradesPerSubject
+    {
+        get
+        {
+            IDictionary<string, List<GradeScale>> gps = Grades.GroupBy(x => x.Subject.Name).ToDictionary(x => x.Key, x => x.Select(x => x.GradeValue).ToList());
+            return gps;
         }
     }
 
     public Student()
     {
         Grades = new List<Grade>();
-        AverageGradePerSubject = new Dictionary<string, double>();
-        GradesPerSubject = new Dictionary<string, List<GradeScale>>();
     }
 }
