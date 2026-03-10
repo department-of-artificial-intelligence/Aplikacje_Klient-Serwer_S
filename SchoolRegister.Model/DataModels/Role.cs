823429760744ee6ac63aabf2df0391064fd3ecd5
Piotr Krzysinski <smth@smth.smth>
2026-03-10 12:26:41 +0100

    lab1-zmiana

--- SchoolRegister.Model/DataModels/Role.cs
+++ SchoolRegister.Model/DataModels/Role.cs
@@ -5,8 +5,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Microsoft.AspNetCore.Identity;
-
 namespace SchoolRegister.Model.DataModels;
 
 public class Role : IdentityRole<int>
