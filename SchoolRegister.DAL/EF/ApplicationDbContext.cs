65260bf7f90d41836698d3ac8d63d86fef2efcc8
Jakub Pilarski <sm.sm@smth.smth>
2026-03-17 14:47:58 +0100

    Lab_02_1

--- SchoolRegister.DAL/EF/ApplicationDbContext.cs
+++ SchoolRegister.DAL/EF/ApplicationDbContext.cs
@@ -35,4 +35,5 @@ namespace SchoolRegister.DAL.EF
             .HasValue<Teacher>((int)RoleValue.Teacher);
         }
     }
+
 }
--- SchoolRegister.Model/DataModels/SubjectGroup.cs
+++ SchoolRegister.Model/DataModels/SubjectGroup.cs
@@ -1,15 +1,19 @@
 using System.Text.RegularExpressions;
 using SchoolRegister.Model.DataModels;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace SchoolRegister.Model.DataModels;
 
 
 public class SubjectGroup
 {
     public Subject Subject { get; set; }
+    [ForeignKey("Subject")]
     public int SubjectId { get; set; }
 
-    public Group Group { get; set; }
-    public int GroupId { get; set; }
+    public virtual Group Group { get; set; }
+    [ForeignKey("Group")]
+    public int? GroupId { get; set; }
 
     public SubjectGroup() { }
 }
