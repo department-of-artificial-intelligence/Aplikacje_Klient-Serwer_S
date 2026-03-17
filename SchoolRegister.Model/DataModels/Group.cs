5c72fac944500a054b891039538e8757d30d6105
Krystian Gornik <smth@smth.smth>
2026-03-17 13:27:53 +0100

    lab2.12

--- SchoolRegister.Model/DataModels/Group.cs
+++ SchoolRegister.Model/DataModels/Group.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Identity;
 using System;
+using System.ComponentModel.DataAnnotations;
 namespace SchoolRegister.Model.DataModels;
 
 public class Group
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; } = null!;
-    public IList<Student> Students { get; set; } = new List<Student>();
+    public virtual IList<Student> Students { get; set; } = new List<Student>();
     public IList<SubjectGroup> SubjectGroups { get; set; } = new List<SubjectGroup>();
 }
--- SchoolRegister.Model/DataModels/Subject.cs
+++ SchoolRegister.Model/DataModels/Subject.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Identity;
 using System;
+using System.ComponentModel.DataAnnotations;
 namespace SchoolRegister.Model.DataModels;
 
 public class Subject
 {
+    [Key]
     public int Id { get; set; }
-    public string Name { get; set; } = null!;
-    public string Description { get; set; } = null!;
+    [Required]
+    public string Name { get; set; }
+    public string Description { get; set; }
     public IList<SubjectGroup> SubjectGroups { get; set; } = new List<SubjectGroup>();
     public Teacher Teacher { get; set; } = null!;
     public int? TeacherId { get; set; }
