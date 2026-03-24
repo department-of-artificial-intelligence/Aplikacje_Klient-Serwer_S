35b6826ee61bb53abe5d391ff3470b5123f79ce4
Mateusz Drej <smth@smth.smth>
2026-03-24 15:38:00 +0100

    lab2 final lab3 start

---
+++ SchoolRegister.Services/Interfaces/ISubjectService.cs
@@ -0,0 +1,20 @@
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SchoolRegister.Model.DataModels;
+using SchoolRegister.Services.Interfaces;
+using SchoolRegister.ViewModels.VM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+namespace SchoolRegister.Services.Interfaces
+
+{
+    public interface ISubjectService
+    {
+        SubjectVm AddOrUpdateSubject(AddOrUpdateSubjectVm addOrUpdateVm);
+        SubjectVm GetSubject(Expression<Func<Subject, bool>> filterExpression);
+        IEnumerable<SubjectVm> GetSubjects(Expression<Func<Subject, bool>> filterExpression = null);
+    }
+}
