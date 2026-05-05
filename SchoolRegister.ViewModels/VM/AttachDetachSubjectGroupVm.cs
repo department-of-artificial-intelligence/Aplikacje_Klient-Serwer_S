63f66fc43cbdbdb855ff20c7e36ba784ebf5bf04
Mateusz Praski <mail@gm4il.pl>
2026-05-05 15:38:55 +0200

    lab4-pdf s.17 05.05.2026

--- SchoolRegister.ViewModels/VM/AttachDetachSubjectGroupVm.cs
+++ SchoolRegister.ViewModels/VM/AttachDetachSubjectGroupVm.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+// AttachDetachSubjectToGroupVm
 namespace SchoolRegister.ViewModels.VM
 {
     public class AttachDetachSubjectGroupVm
--- SchoolRegister.Web/Controllers/SubjectController.cs
+++ SchoolRegister.Web/Controllers/SubjectController.cs
@@ -4,9 +4,12 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
 using SchoolRegister.Model.DataModels;
 using SchoolRegister.Services.Interfaces;
 using SchoolRegister.ViewModels.VM;
+using System.Linq;
+
 namespace SchoolRegister.Web.Controllers;
 
 [Authorize(Roles = "Teacher, Admin, Student")]
@@ -15,31 +18,41 @@ public class SubjectController : BaseController
     private readonly ISubjectService _subjectService;
     private readonly ITeacherService _teacherService;
     private readonly UserManager<User> _userManager;
+
     public SubjectController(ISubjectService subjectService,
-    ITeacherService teacherService,
-    UserManager<User> userManager,
-    IStringLocalizer localizer,
-    ILogger logger,
-    IMapper mapper) : base(logger, mapper, localizer)
+        ITeacherService teacherService,
+        UserManager<User> userManager,
+        IStringLocalizer localizer,
+        ILogger<SubjectController> logger,
+        IMapper mapper) : base(logger, mapper, localizer)
     {
         _subjectService = subjectService;
         _teacherService = teacherService;
         _userManager = userManager;
     }
+
     public IActionResult Index()
     {
         var user = _userManager.GetUserAsync(User).Result;
+        
         if (_userManager.IsInRoleAsync(user, "Admin").Result)
+        {
             return View(_subjectService.GetSubjects());
+        }
         else if (_userManager.IsInRoleAsync(user, "Teacher").Result && user is Teacher teacher)
         {
             return View(_subjectService.GetSubjects(x => x.TeacherId == teacher.Id));
         }
         else if (_userManager.IsInRoleAsync(user, "Student").Result)
+        {
             return RedirectToAction("Details", "Student", new { studentId = user.Id });
+        }
         else
+        {
             return View("Error");
+        }
     }
+
     [HttpGet]
     [Authorize(Roles = "Admin")]
     public IActionResult AddOrEditSubject(int? id = null)
@@ -50,20 +63,30 @@ public class SubjectController : BaseController
             Text = $"{t.FirstName} {t.LastName}",
             Value = t.Id
         }), "Value", "Text");
+
         if (id.HasValue)
         {
             var subjectVm = _subjectService.GetSubject(x => x.Id == id);
             ViewBag.ActionType = "Edit";
             return View(Mapper.Map<AddOrUpdateSubjectVm>(subjectVm));
         }
+        
         ViewBag.ActionType = "Add";
         return View();
     }
+
     public IActionResult Details(int id)
     {
         var subjectVm = _subjectService.GetSubject(x => x.Id == id);
+        
+        if (subjectVm == null)
+        {
+            return NotFound();
+        }
+        
         return View(subjectVm);
     }
+
     [HttpPost]
     [ValidateAntiForgeryToken]
     [Authorize(Roles = "Admin")]
@@ -74,6 +97,16 @@ public class SubjectController : BaseController
             _subjectService.AddOrUpdateSubject(addOrUpdateSubjectVm);
             return RedirectToAction("Index");
         }
-        return View();
+
+        var teachersVm = _teacherService.GetTeachers();
+        ViewBag.TeachersSelectList = new SelectList(teachersVm.Select(t => new
+        {
+            Text = $"{t.FirstName} {t.LastName}",
+            Value = t.Id
+        }), "Value", "Text", addOrUpdateSubjectVm.TeacherId);
+        
+        ViewBag.ActionType = addOrUpdateSubjectVm.Id.HasValue ? "Edit" : "Add";
+        
+        return View(addOrUpdateSubjectVm);
     }
 }
