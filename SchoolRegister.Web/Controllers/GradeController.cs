81cb6b0a65a5f0562351d5543382fb6f9c2bfb8a
Igor Suchanski <igsa@wqaad.csac>
2026-05-05 15:34:52 +0200

    lab04 poprawki 05.05.

--- SchoolRegister.Web/Controllers/GradeController.cs
+++ SchoolRegister.Web/Controllers/GradeController.cs
@@ -4,12 +4,12 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Localization;
-using SchoolRegister.Model.DataModels; 
+using SchoolRegister.Model.DataModels;
 using SchoolRegister.Services.Interfaces;
-using SchoolRegister.ViewModels.VM; 
-using System.Linq; 
-using System; 
-using System.Threading.Tasks; 
+using SchoolRegister.ViewModels.VM;
+using System.Linq;
+using System;
+using System.Threading.Tasks;
 
 namespace SchoolRegister.Web.Controllers;
 
@@ -23,8 +23,8 @@ public class GradeController : BaseController
 
     public GradeController(IGradeService gradeService, ISubjectService subjectService,
                            IStudentService studentService, UserManager<User> userManager,
-                           ILogger<GradeController> logger, IMapper mapper, 
-                           IStringLocalizer<BaseController> localizer) 
+                           ILogger<GradeController> logger, IMapper mapper,
+                           IStringLocalizer<BaseController> localizer)
         : base(logger, mapper, localizer)
     {
         _gradeService = gradeService;
@@ -38,21 +38,23 @@ public class GradeController : BaseController
     public async Task<IActionResult> AddGrade()
     {
         var user = await _userManager.GetUserAsync(User);
-        
+
         // Pobieramy przedmioty przypisane do danego nauczyciela
         var subjects = _subjectService.GetSubjects(s => s.TeacherId == user.Id);
         ViewBag.SubjectsSelectList = new SelectList(subjects, "Id", "Name");
 
         // Pobieramy wszystkich studentów
         var students = _studentService.GetStudents();
-        ViewBag.StudentsSelectList = new SelectList(students.Select(s => new {
+        ViewBag.StudentsSelectList = new SelectList(students.Select(s => new
+        {
             Value = s.Id,
             Text = $"{s.FirstName} {s.LastName}"
         }), "Value", "Text");
 
         // Lista wartości ocen
         var grades = Enum.GetValues(typeof(GradeScale)).Cast<GradeScale>();
-        ViewBag.GradesSelectList = new SelectList(grades.Select(g => new {
+        ViewBag.GradesSelectList = new SelectList(grades.Select(g => new
+        {
             Value = (int)g,
             Text = g.ToString()
         }), "Value", "Text");
@@ -60,16 +62,39 @@ public class GradeController : BaseController
         return View();
     }
 
-    
+
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult AddGrade(AddGradeToStudentVm model)
+    public async Task<IActionResult> AddGrade(AddGradeToStudentVm model)
     {
         if (ModelState.IsValid)
         {
-            _gradeService.AddGradeToStudent(model); 
+            var user = await _userManager.GetUserAsync(User);
+            model.TeacherId = user.Id;
+
+            _gradeService.AddGradeToStudent(model);
             return RedirectToAction("Index", "Subject");
         }
+
+        var currentUser = await _userManager.GetUserAsync(User);
+
+        var subjects = _subjectService.GetSubjects(s => s.TeacherId == currentUser.Id);
+        ViewBag.SubjectsSelectList = new SelectList(subjects, "Id", "Name");
+
+        var students = _studentService.GetStudents();
+        ViewBag.StudentsSelectList = new SelectList(students.Select(s => new
+        {
+            Value = s.Id,
+            Text = $"{s.FirstName} {s.LastName}"
+        }), "Value", "Text");
+
+        var grades = Enum.GetValues(typeof(GradeScale)).Cast<GradeScale>();
+        ViewBag.GradesSelectList = new SelectList(grades.Select(g => new
+        {
+            Value = (int)g,
+            Text = g.ToString()
+        }), "Value", "Text");
+
         return View(model);
     }
 }
