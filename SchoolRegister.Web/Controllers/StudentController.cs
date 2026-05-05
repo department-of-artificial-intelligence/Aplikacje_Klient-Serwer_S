090daccd5a57ed9646bd0ab69f40268b473b0c5e
Mateusz Drej <smth@smth.smth>
2026-05-05 15:40:30 +0200

    lab4 continue 2

---
+++ SchoolRegister.Web/Controllers/StudentController.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Localization;
+using SchoolRegister.Model.DataModels;
+using SchoolRegister.Services.Interfaces;
+using SchoolRegister.ViewModels.VM;
+namespace SchoolRegister.Web.Controllers;
+
+{
+    
+}
