080dde7b0f26ecf89edc1498291f5b9c2988edd6
Piotr Krzysinski <smth@smth.smth>
2026-05-19 13:29:09 +0200

    lab5 poczatek

--- SchoolRegister.Services/ConcreteServices/SubjectService.cs
+++ SchoolRegister.Services/ConcreteServices/SubjectService.cs
@@ -80,5 +80,10 @@ namespace SchoolRegister.Services.ConcreteServices
                 throw;
             }
         }
+
+        public bool RemoveSubject(Expression<Func<Subject, bool>> filterExpression)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
