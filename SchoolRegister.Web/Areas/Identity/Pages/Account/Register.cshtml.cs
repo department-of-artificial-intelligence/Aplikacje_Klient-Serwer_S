676ef1f9f17f95473d472e4bf6c1c41e72b7efaf
Piotr Krzysinski <smth@smth.smth>
2026-05-19 11:49:17 +0200

    zmiany2

--- SchoolRegister.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SchoolRegister.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,16 +1,80 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SchoolRegister.Model.DataModels;
+using System.ComponentModel.DataAnnotations;
 
 namespace SchoolRegister.Web.Areas.Identity.Pages.Account;
 
 public class RegisterModel : PageModel
 {
-    public IActionResult OnGet()
+    private readonly UserManager<User> _userManager;
+    private readonly SignInManager<User> _signInManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+
+    public RegisterModel(
+        UserManager<User> userManager,
+        SignInManager<User> signInManager,
+        RoleManager<IdentityRole> roleManager)
     {
-        return NotFound();
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _roleManager = roleManager;
     }
-    public IActionResult OnPost()
+
+    [BindProperty]
+    public InputModel Input { get; set; } = new();
+
+    public class InputModel
     {
-        return NotFound();
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = "";
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = "";
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; } = "";
+    }
+
+    public void OnGet() { }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+            return Page();
+
+        var user = new User
+        {
+            UserName = Input.Email,
+            Email = Input.Email
+        };
+
+        var result = await _userManager.CreateAsync(user, Input.Password);
+
+        if (result.Succeeded)
+        {
+            // sprawdzenie, czy rola istnieje
+            if (!await _roleManager.RoleExistsAsync("User"))
+            {
+                await _roleManager.CreateAsync(new IdentityRole("User"));
+            }
+
+            // przypisanie roli
+            await _userManager.AddToRoleAsync(user, "User");
+
+            await _signInManager.SignInAsync(user, isPersistent: false);
+
+            return RedirectToPage("/Index");
+        }
+
+        foreach (var error in result.Errors)
+            ModelState.AddModelError(string.Empty, error.Description);
+
+        return Page();
     }
 }
